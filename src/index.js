1cfeb768483f62e2e7efad617e74d426ad407914
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-13 00:58:08 +0500

    clicker React+Redux

--- src/index.js
+++ src/index.js
@@ -1,76 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
-import { createStore } from 'redux'
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-let nextTodoId = 0
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO': {
-      return [
-        ...state,
-        {
-          id: ++nextTodoId,
-          title: action.title,
-          completed: false,
-        },
-      ]
-    }
-
-    case 'TOGGLE_TODO': {
-      return state.map((todo) =>
-        todo.id === action.todoId
-          ? {
-              ...todo,
-              completed: !todo.completed,
-            }
-          : todo
-      )
-    }
-
-    case 'DELETE_TODO': {
-      return state.filter((todo) => todo.id !== action.todoId)
-    }
-
-    default: {
-      return state
-    }
-  }
-}
-
-const store = createStore(todos)
-
-// action creators
-const addTodo = (title) => ({
-  type: 'ADD_TODO',
-  title,
-})
-
-const toggleTodo = (todoId) => ({
-  type: 'TOGGLE_TODO',
-  todoId,
-})
-
-const deleteTodo = (todoId) => ({
-  type: 'DELETE_TODO',
-  todoId,
-})
-
-console.log(store.getState())
-store.dispatch(addTodo('learn react'))
-console.log(store.getState())
-store.dispatch(addTodo('learn redux'))
-console.log(store.getState())
-store.dispatch(toggleTodo(1))
-console.log(store.getState())
-store.dispatch(deleteTodo(2))
-console.log(store.getState())
-
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
