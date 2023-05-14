79a460a7428cf714d88f2b91e8114f0e4d9bb7b6
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-14 23:58:32 +0500

    saving todos to localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,26 @@
 // Точка входа - создание store
 import { createStore } from 'redux'
+import { loadState, saveState } from './localstorage'
 import { rootReducer } from './root-reducer'
+import throttle from 'lodash/throttle'
 
-export const configureStore = () =>
-  createStore(
+export const configureStore = () => {
+  const persistedState = loadState()
+
+  const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        todos: store.getState().todos,
+      })
+    }),
+    1000
+  )
+
+  return store
+}
