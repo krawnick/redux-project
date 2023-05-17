2ce496ba260a49f1212cfc11a7aae069208ec024
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-17 23:14:28 +0500

    add filters reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import { filterReducer } from './filters/filters-reducers'
 import { positionsReducer } from './positions/positions-reducers'
 
 export const rootReducer = combineReducers({
   positions: positionsReducer,
+  filters: filterReducer,
 })
