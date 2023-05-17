bf1800f0ec29ce9326fbc1aabf1d70d9845d6191
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-18 00:17:50 +0500

    react-redux legacy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { FilterPanel } from './components/FilterPanel'
-import { JobList } from './components/JobList'
-import { TheHeader } from './components/TheHeader'
-import { addPositions } from './store/positions/positions-actions'
-import data from './mock/data.json'
-
-function App() {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(addPositions(data))
-  })
-  return (
-    <>
-      <TheHeader />
-      <div className="container">
-        <FilterPanel />
-        <JobList />
-      </div>
-    </>
-  )
+export default function App() {
+  return <div className="App">Hello world</div>
 }
-
-export default App
