434cc6097e864a07ce59255214651be16b6acd8c
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-19 21:00:50 +0500

    add mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import { increment, decrement, reset } from './store'
+
 export default function App() {
-  return <div className="App">Hello world</div>
+  return (
+    <div className="App">
+      <h1>Hello Redux</h1>
+      <Counter />
+    </div>
+  )
 }
+
+class _Counter extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h2>{this.props.count}</h2>
+        <button onClick={this.props.dec}>-</button>
+        <button onClick={this.props.inc}>+</button>
+        <button onClick={this.props.res}>reset</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  count: state.count,
+})
+const mapDispatchToProps = (dispatch) => ({
+  inc: () => dispatch(increment),
+  dec: () => dispatch(decrement),
+  res: () => dispatch(reset),
+})
+
+const Counter = connect(mapStateToProps, mapDispatchToProps)(_Counter)
---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { combineReducers, createStore } from 'redux'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return state + 1
+    }
+    case 'DECREMENT': {
+      return state - 1
+    }
+    case 'RESET': {
+      return 0
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export const store = createStore(
+  combineReducers({ count: counter }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// actions
+
+export const increment = { type: 'INCREMENT' }
+export const decrement = { type: 'DECREMENT' }
+export const reset = { type: 'RESET' }
