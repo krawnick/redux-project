dfaacb14e7f269db0f130e201411eb35899dbb46
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-21 20:54:53 +0500

    project logger-middleware ready

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -17,7 +18,19 @@ const counter = (state = 0, action) => {
   }
 }
 
-export const store = createStore(counter)
+const middleWare = []
+
+if (process.env.NODE_ENV === 'development') {
+  middleWare.push(logger)
+}
+
+// const myLogger = (state) => (next) => (action) => {
+//   console.log('dispatched an action', action.type)
+//   next(action)
+//   console.log('update state is', store.getState())
+// }
+
+export const store = createStore(counter, applyMiddleware(...middleWare))
 
 // actions
 
