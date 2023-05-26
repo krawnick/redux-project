d63bd1e3b611a6684964dd988a2e0ff69aa60957
Nikita Kravtsov <kraw.nick@gmail.com>
2023-05-26 19:44:40 +0500

    edited todos-actions (func createTodo)

--- src/store/todos/todos-actions.js
+++ src/store/todos/todos-actions.js
@@ -3,6 +3,12 @@ import { client } from '../../api'
 export const ADD_TODOS = '@@todos/ADD_TODOS'
 export const SET_LOADING = '@@todos/SET_LOADING'
 export const SET_ERROR = '@@todos/SET_ERROR'
+export const ADD_TODO = '@@todos/ADD_TODO'
+
+const addTodo = (todo) => ({
+  type: ADD_TODO,
+  payload: todo,
+})
 
 const addTodos = (todos) => ({
   type: ADD_TODOS,
@@ -25,4 +31,8 @@ export const loadTodos = () => (dispatch) => {
     .get('https://jsonplaceholder.typicode.com/todos')
     .then((data) => dispatch(addTodos(data)))
     .catch((error) => dispatch(setError(error)))
+}
+
+export const createTodo = (title) => (dispatch) => {
+  client.post()
 }
