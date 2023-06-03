504b682f4d520a62ad65c34fad93df73969b6204
krawnick <kraw.nick@gmail.com>
2023-06-03 17:17:13 +0500

    add actions SET_NEIGHBOURS and loadNei...order

--- src/store/details/details-actions.js
+++ src/store/details/details-actions.js
@@ -2,6 +2,7 @@ export const SET_LOADING = '@@details/SET_LOADING'
 export const SET_ERROR = '@@details/SET_ERROR'
 export const SET_COUNTRY = '@@details/SET_COUNTRY'
 export const CLEAR_DETAILS = '@@details/CLEAR_DETAILS'
+export const SET_NEIGHBOURS = '@@details/SET_NEIGHBOURS'
 
 const setLoading = () => ({
   type: SET_LOADING,
@@ -17,6 +18,11 @@ const setCountry = (country) => ({
   payload: country,
 })
 
+const setNeighbours = (countries) => ({
+  type: SET_NEIGHBOURS,
+  payload: countries,
+})
+
 export const clearDetails = () => ({
   type: CLEAR_DETAILS,
 })
@@ -30,4 +36,15 @@ export const loadCoutryByName =
       .get(api.searchByCountry(name))
       .then(({ data }) => dispatch(setCountry(data[0])))
       .catch((error) => dispatch(setError(error.message)))
+  }
+
+export const loadNeighboursByBorder =
+  (borders) =>
+  (dispatch, _, { client, api }) => {
+    client
+      .get(api.filterByCode(borders))
+      .then(({ data }) =>
+        dispatch(setNeighbours(data.map((country) => country.name)))
+      )
+      .catch(console.error)
   }
