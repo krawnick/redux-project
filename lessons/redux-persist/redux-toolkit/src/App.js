48e26b70a4809a9eebf372f1e350e2f14c570aae
Nikita Kravtsov <kraw.nick@gmail.com>
2023-06-08 18:32:04 +0500

    initialized redux slice

---
+++ lessons/redux-persist/redux-toolkit/src/App.js
@@ -0,0 +1,60 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteTodo, addTodo, toggleTodo } from './store2'
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Redux Todo(redux-toolkit)</h1>
+      <NewTodo />
+      <TodoList />
+    </div>
+  )
+}
+
+const NewTodo = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(addTodo(event.target.title.value))
+    event.target.reset()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" placeholder="new todo..." />
+      <input type="submit" value="Add todo" />
+    </form>
+  )
+}
+
+const TodoList = () => {
+  const dispatch = useDispatch()
+  const todos = useSelector((state) => state)
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              onChange={() => {
+                dispatch(toggleTodo(todo.id))
+              }}
+              checked={todo.completed}
+            />
+            {todo.title}{' '}
+            <button
+              onClick={() => {
+                dispatch(deleteTodo(todo.id))
+              }}
+            >
+              delete
+            </button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
