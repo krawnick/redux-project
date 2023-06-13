c891667d78692a71c0c2d9a27db00081aa8796b2
krawnick <kraw.nick@gmail.com>
2023-06-13 19:41:12 +0500

    add persist to todo(rtk)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import { store } from './store'
+import { persistor, store } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/store.js
+++ src/store.js
@@ -1,18 +1,52 @@
 import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
 import { filterReducer } from './features/Filters/filtes-slice'
 import { todosReducer } from './features/Todos/todos-slice'
+import { combineReducers } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import logger from 'redux-logger'
+
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filterReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   // reducer: todoSlice.reducer,
-  reducer: {
-    todos: todosReducer,
-    filters: filterReducer,
-  },
+  reducer: persistedReducer,
   devTools: true,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // Исключение возможности конфликтов middleware с persist
+      serializableCheck: {
+        FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PERSIST,
+        PURGE,
+        REGISTER,
+      },
+    }).concat(logger),
   enhancers: [],
 })
+
+export const persistor = persistStore(store)
 
 // export const store = createStore(
 //   todoSlice.reducer,
