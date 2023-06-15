80445b9aac1932a5be2de60085de06e9e794a7c2
krawnick <kraw.nick@gmail.com>
2023-06-15 20:07:32 +0500

    refactoring dashboard(rtk) ready

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { filterReducer } from './features/filter/filter-slice'
 import { positionsReducer } from './features/positions/positions-slice'
-import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
